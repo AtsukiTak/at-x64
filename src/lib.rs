be5ed14460a01645e45ca2d0d01e6aded15139d6
AtsukiTak <takatomgoo@gmail.com>
2021-08-05 23:01:21 +0900

    add reg module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod bytecode;
 mod flexbytes;
+mod reg;
 
 pub use bytecode::ByteCode;
 pub use flexbytes::FlexBytes;
+pub use reg::{Reg, Reg16, Reg32, Reg64, Reg8, RegMem16, RegMem32, RegMem64, RegMem8};
